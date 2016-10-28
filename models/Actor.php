595c955c72da78ee0ffbe41ad7d3988254786a71
maru <mix.touch777@gmail.com>
2016-10-28 22:34:08 +0900

    Actorモデルで、関連プロパティと一緒に新規登録するメソッドを作成した

--- models/Actor.php
+++ models/Actor.php
@@ -8,6 +8,9 @@ use yii\db\Expression;
 
 class Actor extends ActiveRecord
 {
+    const TYPE_AV = 1;  //AV女優
+    const TYPE_TV = 2;  //芸能人
+
     public function behaviors(){
         return [
             [
@@ -20,6 +23,48 @@ class Actor extends ActiveRecord
     }
     public function getArticles(){
         return $this->hasMany(Article::className(), ['id' => 'article_id'])->viaTable('article_actor', ['actor_id' => 'id']);
+    }
+
+    static function saveNewAvActor($key_name, $lang_id, $first_name, $middle_name, $last_name, array $thumb_url = []){
+        //todo: 文字数制限チェックと画像urlの登録
+        $profiles = ['first_name', 'middle_name', 'last_name'];
+        $transaction = self::getDb()->beginTransaction();
+        try{
+            //女優を新規登録
+            $actor = new Actor();
+            $actor->key_name = $key_name;
+            $actor->type = self::TYPE_AV;
+            $actor->save();
+
+            //名前を新規登録
+            $name_ids = [];
+            foreach($profiles as $profile){
+                if(!empty($$profile)){
+                    $text = new Text();
+                    $text->text = $$profile;
+                    $text->save();
+                    $name_ids[$profile] = $text->id;
+                }
+            }
+
+            //女優プロフィール関連テーブルに保存
+            foreach($profiles as $profile){
+                if(!empty($$profile)){
+                    $text_property = TextProperty::findOne(['name' => "actor_".$profile]);
+                    $actor_text_profile = new ActorTextProperty();
+                    $actor_text_profile->actor_id = $actor->id;
+                    $actor_text_profile->text_property_id = $text_property->id;
+                    $actor_text_profile->language_id = $lang_id;
+                    $actor_text_profile->text_id = $name_ids[$profile];
+                    $actor_text_profile->save();
+                }
+            }
+        }catch(\Exception $e){
+            $transaction->rollBack();
+            throw $e;
+        }
+        $transaction->commit();
+        return true;
     }
 
     /**
---
+++ models/ActorTextProperty.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+class ActorTextProperty extends ActiveRecord
+{
+    public function behaviors(){
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created',
+                'updatedAtAttribute' => 'updated',
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+}
