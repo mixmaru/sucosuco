6b91880c4d07edd6bcc1a50bbeced5fe0656b713
maru <mix.touch777@gmail.com>
2016-10-30 17:31:40 +0900

    Actorモデルにて、新規追加メソッド（DAO版）が動作するようになった

--- models/Actor.php
+++ models/Actor.php
@@ -12,6 +12,8 @@ class Actor extends Model {
     public $middle_name;
     public $last_name;
     public $profile_image_paths = [];
+    public $created;
+    public $updated;
 
     public static function getInstanceById($id, $language_id){
         $actor = new Actor();
@@ -42,5 +44,104 @@ class Actor extends Model {
         $this->first_name = $param['first_name'];
         $this->middle_name = $param['middle_name'];
         $this->last_name = $param['last_name'];
+    }
+
+    public function save($language_id){
+        if(!self::existId($this->id)){
+            $this->create($language_id);
+        }else{
+            $this->update($language_id);
+        }
+    }
+
+    public static function existId($id){
+        $sql = "SELECT id FROM actor WHERE id = :id";
+        $result = \Yii::$app->db->createCommand($sql, [':id' => $id])->queryOne();
+        if($result === false){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private function create($language_id){
+        $db = \Yii::$app->db;
+        $did_id_null = is_null($this->id);
+        $now = date('Y-m-d H:i:s');
+        $actor_sql = "INSERT INTO actor (type, key_name, created, updated) VALUES (:type, :key_name, :created, :updated)";
+        $param = [
+            ':type' => $this->type,
+            ':key_name' => $this->key_name,
+            ':created' => $now,
+            ':updated' => $now,
+        ];
+        $transaction = $db->beginTransaction();
+        try{
+            $db->createCommand($actor_sql, $param)->execute();
+            if($did_id_null){
+                $this->id = $db->getLastInsertID();
+            }
+
+            //textにfirst_name, middle_name, last_nameを保存して、それぞれのidを保持
+            $text_insert_command = $db->createCommand("INSERT INTO text (text, created, updated) VALUES (:text, :created, :updated)");
+            $actor_text_property_insert_command = $db->createCommand("INSERT INTO actor_text_property (actor_id, text_property_id, language_id, text_id, created, updated) VALUES (:actor_id, :text_property_id, :language_id, :text_id, :created, :updated)");
+            foreach($this->getTextPropertyNames() as $text_property_name){
+                if(is_null($this->$text_property_name)){
+                    continue;
+                }
+                $text_property_obj = TextProperty::getInstanceFromName("actor_".$text_property_name);
+                $text_insert_command->bindValues([
+                    ':text' => $this->$text_property_name,
+                    ':created' => $now,
+                    ':updated' => $now,
+                ]);
+                $text_insert_command->execute();
+                //id取得
+                $text_id = $db->getLastInsertID();
+                $actor_text_property_insert_command->bindValues([
+                    ':actor_id' => $this->id,
+                    ':text_property_id' => $text_property_obj->id,
+                    ':language_id' => $language_id,
+                    ':text_id' => $text_id,
+                    ':created' => $now,
+                    ':updated' => $now,
+                ]);
+                $actor_text_property_insert_command->execute();
+            }
+        }catch(\Exception $e){
+            $transaction->rollBack();
+            if($did_id_null){
+                $this->id = null;
+            }
+            throw $e;
+        }
+        $this->created = $now;
+        $this->updated = $now;
+        $transaction->commit();
+    }
+
+    private function update($language_id){
+        $now = date('Y-m-d H:i:s');
+        $sql = "UPDATE actor SET type = :type, key_name = :key_name, updated = :updated WHERE id = :id";
+        $param = [
+            ':id' => $this->id,
+            ':type' => $this->type,
+            ':key_name' => $this->key_name,
+            ':updated' => $now,
+        ];
+        try{
+            \Yii::$app->db->createCommand($sql, $param)->execute();
+        }catch(\Exception $e){
+            throw $e;
+        }
+        $this->updated = $now;
+    }
+
+    private function getTextPropertyNames(){
+        return [
+            'first_name',
+            'middle_name',
+            'last_name',
+        ];
     }
 }
--- models/TextProperty.php
+++ models/TextProperty.php
@@ -1,21 +1,31 @@
 <?php
 
 namespace app\models;
+use yii\base\Model;
 
-use yii\db\ActiveRecord;
-use yii\behaviors\TimestampBehavior;
-use yii\db\Expression;
-
-class TextProperty extends ActiveRecord
+class TextProperty extends Model
 {
-    public function behaviors(){
-        return [
-            [
-                'class' => TimestampBehavior::className(),
-                'createdAtAttribute' => 'created',
-                'updatedAtAttribute' => 'updated',
-                'value' => new Expression('NOW()'),
-            ],
-        ];
+    public $id;
+    public $name;
+    public $text_limit;
+
+    static private $cache = [];//テーブルデータをクラスキャッシュ
+
+    static public function getInstanceFromName($name){
+        $ret_obj = false;
+        if(empty(self::$cache)){
+            self::loadCache();
+        }
+        foreach(self::$cache as $data){
+            if($data['name'] == $name){
+                $ret_obj = new self($data);
+            }
+        }
+        return $ret_obj;
+    }
+
+    private static function loadCache(){
+        $db = \Yii::$app->db;
+        self::$cache = $db->createCommand("SELECT id, name, text_limit FROM text_property")->queryAll();
     }
 }
